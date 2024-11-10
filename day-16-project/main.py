4a75c99a5d5de52e3ee651bb1f6a0c518f1956c3
steph-viereckl <steph.ralls@gmail.com>
2024-11-10 13:20:53 -0700

    import oop coffee maker code

--- day-16-project/main.py
+++ day-16-project/main.py
@@ -1,1 +1,14 @@
-import turtle
+# https://docs.python.org/3/library/turtle.html
+from turtle import Turtle, Screen
+import prettytable
+
+# To install package... PyCharm > Settings > Click on Project > Python Interpreter > "+" > Find package and install
+
+# Create object/instance from class
+table = prettytable.PrettyTable()
+
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
