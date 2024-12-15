04440c355ab4a62a73a1f078b3fe68a1440d71bb
steph-viereckl <steph.ralls@gmail.com>
2024-12-15 15:27:32 -0700

    Improved and cleaned up code

--- day-25-us-states-game-start/label.py
+++ day-25-us-states-game-start/label.py
@@ -8,21 +8,12 @@ class StateLabel(Turtle):
 
     def __init__(self, state_df):
         super().__init__()
-        # Get x cor from series and y cor
-
-        self.hideturtle()
-        self.penup()
-        self.color("black")
-
-        print(f"state.state: {state_df.state.item()}")
 
         state_name = state_df.state.item()
         x_cor = state_df.x.item()
         y_cor = state_df.y.item()
 
-        print(f"state.x: {x_cor}")
-        print(f"state.y: {y_cor}")
-        print(f"state.state: {state_name}")
-
+        self.hideturtle()
+        self.penup()
         self.setposition(x_cor, y_cor)
         self.write(align=ALIGNMENT, move=False, arg=state_name, font=FONT)
