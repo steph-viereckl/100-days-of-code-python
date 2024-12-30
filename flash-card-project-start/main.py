c76e75c77b001ec7d5a276c79100221645b5fd46
steph-viereckl <steph.ralls@gmail.com>
2024-12-30 11:05:17 -0700

    In the middle of step 3

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -3,17 +3,47 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+MILLISECONDS = 3000
+timer = None
+current_words = {}
 
 def mark_incorrect():
-    word_label.config(text=random.choice(french_words)["French"])
+    pass
 
 def mark_correct():
-    word_label.config(text=random.choice(french_words)["French"])
+    pass
+
+def get_words():
+    global current_words
+    current_words = random.choice(word_dict)
+
+def set_front():
+    pass
+
+def show_card(is_front):
+
+    if is_front is True:
+        get_words()
+        card_front_img = PhotoImage(file="images/card_front.png")
+        canvas.create_image(400, 263, image=card_front_img)
+        title_label.config(text="French")
+        word_label.config(text=current_words["French"])
+
+    else:
+        title_label.config(text="English")
+        word_label.config(text=current_words["English"])
 
 
+def count_down(is_front):
+    print("In count down")
+    show_card(is_front)
+
+    global timer
+    timer = window.after(MILLISECONDS, count_down, not is_front)
+
+# Read CSV File
 word_data_frame = pandas.read_csv("data/french_words.csv")
-french_words = word_data_frame.to_dict(orient="records")
-print(french_words)
+word_dict = word_data_frame.to_dict(orient="records")
 
 # Window
 window = Tk()
@@ -23,16 +53,19 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # Card Front
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
 # Need to define the x position and y position (to center image)
 canvas.create_image(400, 263, image = card_front_img)
 canvas.grid(column=0, row=0, columnspan=2)
 
+# TODO need to dynamically set front image and then switch it 
+
 # Title Label
-title_label = Label(text="French", font=("Arial", 40, "italic"), bg="white")
+title_label = Label(font=("Arial", 40, "italic"), bg="white")
 title_label.place(x=400, y=150, anchor=CENTER)
 
 # Title Label
-word_label = Label(text=random.choice(french_words)["French"], font=("Arial", 60, "bold"), bg="white")
+word_label = Label(font=("Arial", 60, "bold"), bg="white")
 word_label.place(x=400, y=263, anchor=CENTER)
 
 # Incorrect Button
@@ -44,5 +77,8 @@ incorrect_button.grid(column=0, row=1)
 correct_img = PhotoImage(file="images/right.png")
 correct_button = Button(image=correct_img, command=mark_correct, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
 correct_button.grid(column=1, row=1)
+
+# Select the card to show
+# count_down(True)
 
 window.mainloop()
