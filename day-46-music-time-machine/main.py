9f996c114a2596ca7f2979687f170de011d84099
steph-viereckl <steph.ralls@gmail.com>
2025-01-27 15:06:45 -0700

    Stuck but will look more tomorrow

--- day-46-music-time-machine/main.py
+++ day-46-music-time-machine/main.py
@@ -1,7 +1,16 @@
+from dataclasses import dataclass
+
 from bs4 import BeautifulSoup
 import requests
+import os
+from dotenv import load_dotenv
+import spotipy
+from pandas.core.missing import clean_interp_method
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
-
+from token import tok_name
+load_dotenv()
 
 # Ask user to enter Date
 # input_date = input("Which year do you want to travel to? Type the dat in this format: YYYY-MM-DD:")
@@ -26,4 +35,34 @@ for tag in song_tags:
     # For each tag, get the text and strip of new lines/breaks
     song_list.append(tag.getText().strip())
 
-print(f"song list: {song_list}")
+# print(f"song list: {song_list}")
+with open('token.txt', 'r') as data:
+    token = data.read()
+    print(token)
+
+
+client_id = os.environ.get("CLIENT_ID", "Spotify Client Id cannot be found")
+client_secret = os.environ.get("CLIENT_SECRET", "Spotify Client Secret cannot be found")
+
+# auth_response = spotipy.oauth2.SpotifyOAuth(
+#     client_id=client_id,
+#     client_secret=client_secret,
+#     redirect_uri="http://example.com",
+#     scope="playlist-modify-private"
+# )
+#
+# print(auth_response.get_auth_response())
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=client_id,
+        client_secret=client_secret,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+
+print(f"sp: {sp}")
+print(f"sp user: {sp.current_user()}")
