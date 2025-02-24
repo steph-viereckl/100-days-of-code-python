6087ca75d033ef3ed2dbf8eeb8e5eed064ca94b7
steph-viereckl <steph.ralls@gmail.com>
2025-02-24 10:17:11 -0700

    Started movie list website

---
+++ day-64-starting-files-top-movies/main.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+# Setup path to database on the Flask App
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies.db"
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# initialise the app with the extension
+db.init_app(app)
+
+# CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[float] = mapped_column(Float, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+
+    # READ ALL RECORDS
+    # Construct a query to select from the database. Returns the rows in the database
+    result = db.session.execute(db.select(Movie).order_by(Movie.title))
+    # Use .scalars() to get the elements rather than entire rows from the database
+    movies = result.scalars().all()
+    return render_template("index.html", movie_list=movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
